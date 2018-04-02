e0014e49d948d975eb2ac45b9906b724b4a63d14
Diego <dicolomb@microsoft.com>
2018-04-02 18:28:17 +0100

    add externals

--- rollup.config.js
+++ rollup.config.js
@@ -5,65 +5,68 @@ import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  input: 'src/main.ts',
-  output: {
-    file: 'out/main.js',
-    format: 'cjs',
-    sourcemap: 'inline'
-  },
-  plugins: [
-    commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      include: 'node_modules/**',  // Default: undefined
-    //   exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
-      // these values can also be regular expressions
-      // include: /node_modules/
+    input: 'src/main.ts',
+    external: [
+        "vscode"
+    ],
+    output: {
+        file: 'out/main.js',
+        format: 'cjs',
+        sourcemap: 'inline'
+    },
+    plugins: [
+        commonjs({
+            // non-CommonJS modules will be ignored, but you can also
+            // specifically include/exclude files
+            include: 'node_modules/**',  // Default: undefined
+            //   exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
+            // these values can also be regular expressions
+            // include: /node_modules/
 
-      // search for files other than .js files (must already
-      // be transpiled by a previous plugin!)
-    //   extensions: [ '.js', '.coffee' ],  // Default: [ '.js' ]
+            // search for files other than .js files (must already
+            // be transpiled by a previous plugin!)
+            //   extensions: [ '.js', '.coffee' ],  // Default: [ '.js' ]
 
-      // if true then uses of `global` won't be dealt with by this plugin
-    //   ignoreGlobal: false,  // Default: false
+            // if true then uses of `global` won't be dealt with by this plugin
+            //   ignoreGlobal: false,  // Default: false
 
-      // if false then skip sourceMap generation for CommonJS modules
-    //   sourceMap: false,  // Default: true
+            // if false then skip sourceMap generation for CommonJS modules
+            //   sourceMap: false,  // Default: true
 
-      // explicitly specify unresolvable named exports
-      // (see below for more details)
-    //   namedExports: { './module.js': ['foo', 'bar' ] },  // Default: undefined
+            // explicitly specify unresolvable named exports
+            // (see below for more details)
+            //   namedExports: { './module.js': ['foo', 'bar' ] },  // Default: undefined
 
-      // sometimes you have to leave require statements
-      // unconverted. Pass an array containing the IDs
-      // or a `id => boolean` function. Only use this
-      // option if you know what you're doing!
-    //   ignore: [ 'conditional-runtime-dependency' ]
-    }),
-    json({
-        // All JSON files will be parsed by default,
-        // but you can also specifically include/exclude files
-        include: 'node_modules/**',
-        
-        // for tree-shaking, properties will be declared as
-        // variables, using either `var` or `const`
-        preferConst: true, // Default: false
-  
-        // specify indentation for the generated default export —
-        // defaults to '\t'
-        indent: '  '
-      }),
-    nodeResolve({
-      jsnext: true,
-      main: true
-    }),
-    typescript({
-        tsconfigOverride: {
-            compilerOptions: {
-                module: "ES2015"
+            // sometimes you have to leave require statements
+            // unconverted. Pass an array containing the IDs
+            // or a `id => boolean` function. Only use this
+            // option if you know what you're doing!
+            //   ignore: [ 'conditional-runtime-dependency' ]
+        }),
+        json({
+            // All JSON files will be parsed by default,
+            // but you can also specifically include/exclude files
+            include: 'node_modules/**',
+
+            // for tree-shaking, properties will be declared as
+            // variables, using either `var` or `const`
+            preferConst: true, // Default: false
+
+            // specify indentation for the generated default export —
+            // defaults to '\t'
+            indent: '  '
+        }),
+        nodeResolve({
+            jsnext: true,
+            main: true
+        }),
+        typescript({
+            tsconfigOverride: {
+                compilerOptions: {
+                    module: "ES2015"
+                }
             }
-        }
-    }),
-    //uglify()
-  ]
+        }),
+        //uglify()
+    ]
 };
